574d662312dfe48008ee925a82916a2cfe92b077
fggt <gonzava@gmail.com>
2023-07-29 14:01:34 -0400

    mejoramiento masivo proyecto

--- config/settings.py
+++ config/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'laboratorio',
-    'bootstrap_themes',
+    'bootstrap5',
     
 ]
 
@@ -124,6 +124,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
