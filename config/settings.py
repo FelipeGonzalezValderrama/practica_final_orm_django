4ad70290bf6d0f9174cc4af39878ae2bba91bc04
fggt <gonzava@gmail.com>
2023-08-25 08:57:17 -0400

    deploy settings

--- config/settings.py
+++ config/settings.py
@@ -126,8 +126,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
-
 if not DEBUG:
     # Tell Django to copy statics to the `staticfiles` directory
     # in your application directory on Render.
